4b954fad2b670a97cef875283dccf26f548414fa
WithAbdullah <withmabdullah@gmail.com>
2020-06-09 01:27:44 +0500

    update name

--- src/myportfolio.js
+++ src/myportfolio.js
@@ -3,7 +3,7 @@ import './App.css';
 function Name(props){
     return(
         <div>
-    <h1>My name is {props.Myname}</h1>
+    <h1>Assignment delivered by{props.Myname} CNC Bootcamp2020 Student</h1>
     <img src="/images/abdullah.jpg" alt="abdullah" />
     </div>
 
